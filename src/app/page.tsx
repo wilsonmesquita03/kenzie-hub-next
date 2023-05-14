4aff247d2a4ad0890ec8bf2615a7b99de833c44a
Wilson Mesquita <wilsonbrasilplauforever@gmail.com>
2023-05-14 16:31:19 -0300

    fix: location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 import Background from '@/components/Background'
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 export default function Home() {
   const router = useRouter()
 
-  router.push("/login")
+  useEffect(() => {
+    router.push("/login")
+  }, [router])
 
   return (
     <Background/>
